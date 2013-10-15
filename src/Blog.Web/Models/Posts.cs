0e9f21d144dbbdbcf6d3d1d5970a36d0ba584979
Kijana Woodard <github@wyldeye.com>
2013-10-15 10:58:57 -0500

    finished

--- src/Blog.Web/Models/Posts.cs
+++ src/Blog.Web/Models/Posts.cs
@@ -3,8 +3,6 @@ using Blog.Web.Core;
 
 namespace Blog.Web.Models
 {
-	//Foo - IFoo
-
 	//why not put the dependency in the method - is the func even necessary?
 	//why can't an interface be declared for a method parameter
 
