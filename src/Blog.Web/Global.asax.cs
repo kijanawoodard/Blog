0f4d2566f2347b465f02a66e9a694d0f6b9651d7
Kijana Woodard <github@wyldeye.com>
2013-11-05 22:49:03 -0600

    confirm errors still ysod
    clean up end request

--- src/Blog.Web/Global.asax.cs
+++ src/Blog.Web/Global.asax.cs
@@ -1,9 +1,5 @@
-﻿using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.WebPages;
-using Blog.Web.Actions.DisplayErrors;
 using Blog.Web.Infrastructure;
 using Blog.Web.Initialization;
 
@@ -22,21 +18,5 @@ namespace Blog.Web
 			ViewEngines.Engines.Clear();
 			ViewEngines.Engines.Add(new AlternateViewEngine());
 		}
-
-		//http://stackoverflow.com/a/9026907/214073
-//		protected void Application_EndRequest()
-//		{
-//			if (Context.Response.StatusCode == 404)
-//			{
-//				Response.Clear();
-//
-//				var rd = new RouteData();
-//				rd.Values["controller"] = "DisplayErrors";
-//				rd.Values["action"] = "Http404";
-//
-//				IController c = new DisplayErrorsController();
-//				c.Execute(new RequestContext(new HttpContextWrapper(Context), rd));
-//			}
-//		}
 	}
 }
