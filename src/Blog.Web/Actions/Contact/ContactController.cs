d4d6aef790f61c85ee1ecd8975c3a155def60f77
Kijana Woodard <github@wyldeye.com>
2016-08-01 15:36:02 -0500

    enhance honeypot

--- src/Blog.Web/Actions/Contact/ContactController.cs
+++ src/Blog.Web/Actions/Contact/ContactController.cs
@@ -14,14 +14,15 @@ namespace Blog.Web.Actions.Contact
             return View();
         }
 
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Index(PostModel model)
         {
-            var isbot = !string.IsNullOrWhiteSpace(model.Access);
-            var access = isbot ? $"BOT Access: {model.Access}\n\n" : "";
+            var isbot = !string.IsNullOrWhiteSpace(model.Instructions) || model.Confirm;
+            var botSubject = isbot ? "[BOT] " : "";
+            var botBody = isbot ? $"BOT Instructions: {model.Instructions}\nBot confirm: {model.Confirm}\n\n" : "";
             var message = new MailMessage("contact-form@kijanawoodard.com", "contact-form@kijanawoodard.com");
-            message.Subject = $"[Blog Contact] from {model.Name}";
-            message.Body = access + model.Text;
+            message.Subject = $"{botSubject}[Blog Contact] from {model.Name}";
+            message.Body = botBody + model.Text;
             message.ReplyToList.Add(model.Email);
 
             var smtp = new SmtpClient(AppSettings["smtp::host"], int.Parse(AppSettings["smtp::port"]));
@@ -44,7 +45,8 @@ namespace Blog.Web.Actions.Contact
             public string Name { get; set; }
             public string Email { get; set; }
             public string Text { get; set; }
-            public string Access { get; set; }
+            public string Instructions { get; set; }
+            public bool Confirm { get; set; }
         }
     }
 }
