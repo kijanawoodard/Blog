14c26b8534dfd821f59e694028c253b812f6bc39
Kijana Woodard <github@wyldeye.com>
2016-08-06 16:46:17 -0500

    add a little more honey

--- src/Blog.Web/Actions/Contact/ContactController.cs
+++ src/Blog.Web/Actions/Contact/ContactController.cs
@@ -17,13 +17,15 @@ namespace Blog.Web.Actions.Contact
         [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Index(PostModel model)
         {
-            var isbot = !string.IsNullOrWhiteSpace(model.Instructions) || model.Confirm;
+            var isbot = !string.IsNullOrWhiteSpace(model.Message) || !string.IsNullOrWhiteSpace(model.Instructions) || model.Confirm;
             var botSubject = isbot ? "[BOT] " : "";
-            var botBody = isbot ? $"BOT Instructions: {model.Instructions}\nBot confirm: {model.Confirm}\n\n" : "";
+            var botBody = isbot ? $"BOT Message:{model.Message}\nInstructions: {model.Instructions}\nBot confirm: {model.Confirm}\n\n" : "";
             var message = new MailMessage("contact-form@kijanawoodard.com", "contact-form@kijanawoodard.com");
             message.Subject = $"{botSubject}[Blog Contact] from {model.Name}";
             message.Body = botBody + model.Text;
-            message.ReplyToList.Add(model.Email);
+
+            if (!string.IsNullOrWhiteSpace(model.Email))
+                message.ReplyToList.Add(model.Email);
 
             var smtp = new SmtpClient(AppSettings["smtp::host"], int.Parse(AppSettings["smtp::port"]));
             smtp.Credentials = new NetworkCredential(AppSettings["smtp::username"], AppSettings["smtp::password"]);
@@ -45,6 +47,7 @@ namespace Blog.Web.Actions.Contact
             public string Name { get; set; }
             public string Email { get; set; }
             public string Text { get; set; }
+            public string Message { get; set; }
             public string Instructions { get; set; }
             public bool Confirm { get; set; }
         }
