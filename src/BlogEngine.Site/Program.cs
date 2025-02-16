577d7900403050069140c232f88b4ddd7e461b9d
Kijana Woodard <kijana@kijanawoodard.com>
2025-02-15 22:01:04 -0600

    bring over css, fonts add header, footer

--- src/BlogEngine.Site/Program.cs
+++ src/BlogEngine.Site/Program.cs
@@ -72,3 +72,24 @@ public static class StringExtensions
         return hashedString;
     }
 }
+
+public static class AssemblyExtensions
+    {
+        public static AssemblyInfo GetAssemblyInfo()
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            var fileInfo = new System.IO.FileInfo(assembly.Location);
+            
+            return new AssemblyInfo
+            {
+                Version = assembly.GetName().Version,
+                LastModified = fileInfo.LastWriteTime
+            };
+        }
+
+        public class AssemblyInfo
+        {
+            public Version Version { get; set; }
+            public DateTime LastModified { get; set; }
+        }
+    }
