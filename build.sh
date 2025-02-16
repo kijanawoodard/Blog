660b75ea7a97f6546482bd3a0b66f8fee5e1c82c
Kijana Woodard <kijana@kijanawoodard.com>
2025-02-15 20:06:33 -0600

    run the site instead of publish

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
-./dotnet/dotnet publish -c Release -o output
+./dotnet/dotnet run --project ./src/BlogEngine.Site/
